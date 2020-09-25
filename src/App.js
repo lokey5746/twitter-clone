02c85e6b455369484d246098cb62575a9062eba0
Lokey Dangi <lokendradangi0@gmail.com>
2020-09-25 04:08:17 -0700

    app cleanup is done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h2>Twitter Clone</h2>
+    </div>
+  );
+}
+
+export default App;
